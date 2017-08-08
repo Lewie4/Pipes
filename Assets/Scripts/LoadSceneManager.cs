a4a87ade93257949f6cff31a9cf5102fb6354818
Matilde Herland <matildeherland@gmail.com>
2017-08-08 20:18:34 +0100

    Second commit

---
+++ Assets/Scripts/LoadSceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : MonoBehaviour 
+{
+    public static LoadSceneManager Instance = null;
+
+    public void Awake()
+    {
+        /*if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }*/
+    }
+
+    public void LoadSceneByName (string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
