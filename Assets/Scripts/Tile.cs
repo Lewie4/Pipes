35ab25cebd4807fb11a9a76fa49596d5050710aa
Matilde Herland <matildeherland@gmail.com>
2017-08-08 20:18:15 +0100

    First commit

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour 
+{
+    public enum TileType
+    {
+        Empty,
+        Corner, 
+        Line, 
+        Cross, 
+        Threeway
+    };
+
+    [System.Serializable]
+    public class TileProperties
+    {
+        public TileType m_tileType;
+        public bool m_top;
+        public bool m_left;
+        public bool m_right;
+        public bool m_bottom;
+        public bool m_isFull;
+    }
+
+    [SerializeField] private TileProperties m_tileProperties;
+
+    [SerializeField] private Image m_pipeImage;
+    [SerializeField] private Sprite m_emptyImage;
+    [SerializeField] private Sprite m_fullImage;
+
+    public void PipeFill()
+    {
+        m_pipeImage.sprite = m_fullImage;
+        m_tileProperties.m_isFull = true;
+    }
+
+    public void PipeEmpty()
+    {
+        m_pipeImage.sprite = m_emptyImage;
+        m_tileProperties.m_isFull = false;
+    }
+
+    public void PipeRotateCW()
+    {
+        if (!m_tileProperties.m_isFull)
+        {
+            bool temp = m_tileProperties.m_right;
+            m_tileProperties.m_right = m_tileProperties.m_top;
+            m_tileProperties.m_top = m_tileProperties.m_left;
+            m_tileProperties.m_left = m_tileProperties.m_bottom;
+            m_tileProperties.m_bottom = temp;
+
+            Quaternion newRot = Quaternion.Euler(0, 0, this.transform.localRotation.eulerAngles.z - 90);
+
+            this.transform.localRotation = newRot;
+        }
+    }
+
+    public TileType GetTileType()
+    {
+        return m_tileProperties.m_tileType;
+    }
+
+    public bool GetTop()
+    {
+        return m_tileProperties.m_top;
+    }
+
+    public void SetTop(bool active)
+    {
+        m_tileProperties.m_top = active;
+    }
+
+    public bool GetLeft()
+    {
+        return m_tileProperties.m_left;
+    }
+
+    public void SetLeft(bool active)
+    {
+        m_tileProperties.m_left = active;
+    }
+
+    public bool GetRight()
+    {
+        return m_tileProperties.m_right;
+    }
+
+    public void SetRight(bool active)
+    {
+        m_tileProperties.m_right = active;
+    }
+
+    public bool GetBottom()
+    {
+        return m_tileProperties.m_bottom;
+    }
+
+    public void SetBottom(bool active)
+    {
+        m_tileProperties.m_bottom = active;
+    }
+
+    public bool GetIsFull()
+    {
+        return m_tileProperties.m_isFull;
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,338 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileManager : MonoBehaviour
+{
+    public enum FillLocation
+    {
+        None,
+        Top, 
+        Left, 
+        Right, 
+        Bottom
+    };
+
+    [System.Serializable]
+    public class Column
+    {
+        public List<Tile> m_column = new List<Tile>();
+    }
+
+    [System.Serializable]
+    public class PipesToFill
+    {
+        public Vector2 m_location = Vector2.zero;
+        public FillLocation m_filledFrom = FillLocation.None;
+
+        public PipesToFill(Vector2 loc, FillLocation from)
+        {
+            m_location = loc;
+            m_filledFrom = from;
+        }
+    }
+
+    [SerializeField] private GameObject m_startContainer;
+    [SerializeField] private List<Tile> m_startTiles = new List<Tile>();
+    [SerializeField] private GameObject m_endContainer;
+    [SerializeField] private List<Tile> m_endTiles = new List<Tile>();
+
+    [SerializeField] private List<Column> m_gameBoard = new List<Column>();
+    [SerializeField] private float m_tileSize = 220f;
+
+    private RectTransform RT;
+    private float m_scale;
+    private float m_halfScale;
+    private float m_sizeScale;
+
+    [SerializeField] private float m_timeToStart = 30f;
+    [SerializeField] private float m_timeToFill = 3f;
+    [SerializeField] private List<PipesToFill> m_pipesToFill = new List<PipesToFill>();
+ 
+    private bool m_isFilling = false;
+    private float m_startFillTime = 0;
+    private float m_currentFillTime = 0;
+
+    [SerializeField] private bool m_hasWon = false;
+    [SerializeField] private GameObject m_gameWonObject;
+
+    [SerializeField] private bool m_hasLost = false;
+    [SerializeField] private GameObject m_gameLostObject;
+
+    private void Awake()
+    {
+        SetBoardSize();
+        PositionTiles();
+        PositionStartTiles();
+        PositionEndTiles();
+    }
+
+    private void SetBoardSize()
+    {
+        RT = this.GetComponent<RectTransform>();
+        RectTransform parentRT = this.transform.parent.GetComponent<RectTransform>();
+
+        Vector2 parentSize = new Vector2(parentRT.rect.width, parentRT.rect.height);
+
+        float diff = 0;
+        if (parentSize.x >= parentSize.y)
+        {
+            RT.sizeDelta = new Vector2(parentSize.y, parentSize.y);
+            diff = parentSize.x - parentSize.y;
+            RT.position = new Vector3(diff / 2, 0, 0);
+        }
+        else
+        {
+            RT.sizeDelta = new Vector2(parentSize.x, parentSize.x);
+            diff = parentSize.y - parentSize.x;
+            RT.position = new Vector3(0, diff / 2, 0);
+        }
+
+    }
+
+    private void PositionTiles()
+    {
+        m_scale = RT.sizeDelta.x / m_gameBoard.Count;
+        m_halfScale = m_scale / 2;
+
+        m_sizeScale = m_scale / m_tileSize;
+
+        for (int i = 0; i < m_gameBoard.Count; i++)
+        {
+            for (int j = 0; j < m_gameBoard[i].m_column.Count; j++)
+            {
+                m_gameBoard[i].m_column[j].transform.localPosition = new Vector3(m_halfScale + (i * m_scale), m_halfScale + (j * m_scale), 0);
+                m_gameBoard[i].m_column[j].transform.localScale = new Vector3(m_sizeScale, m_sizeScale, 0);
+            }
+        }
+    }
+
+    private void PositionStartTiles()
+    {
+        RectTransform startRT = m_startContainer.GetComponent<RectTransform>();
+        startRT.sizeDelta = new Vector2(m_scale, RT.sizeDelta.x);
+        float currentY = -startRT.sizeDelta.y / 2;
+
+        for (int i = 0; i < m_startTiles.Count; i++)
+        {
+            if (m_startTiles[i] != null)
+            {
+                m_startTiles[i].transform.localPosition = new Vector3(-m_halfScale, currentY + m_halfScale + (i * m_scale), 0);
+                m_startTiles[i].transform.localScale = new Vector3(m_sizeScale, m_sizeScale, 0);
+                m_startTiles[i].PipeFill();
+
+                m_pipesToFill.Add(new PipesToFill(new Vector2(0, i), FillLocation.Left));
+            }
+        }
+    }
+
+    private void PositionEndTiles()
+    {
+        RectTransform endRT = m_endContainer.GetComponent<RectTransform>();
+        endRT.sizeDelta = new Vector2(m_scale, RT.sizeDelta.x);
+        float currentY = -endRT.sizeDelta.y / 2;
+
+        for (int i = 0; i < m_endTiles.Count; i++)
+        {
+            if (m_endTiles[i] != null)
+            {
+                m_endTiles[i].transform.localPosition = new Vector3(m_halfScale, currentY + m_halfScale + (i * m_scale), 0);
+                m_endTiles[i].transform.localScale = new Vector3(m_sizeScale, m_sizeScale, 0);
+                m_endTiles[i].GetComponent<Button>().enabled = false;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (!m_hasLost)
+        {
+            if (m_hasWon)
+            {
+                if (m_gameWonObject != null && !m_gameWonObject.activeInHierarchy)
+                {
+                    m_gameWonObject.SetActive(true);
+                }
+            }
+            else
+            {
+                if (m_isFilling)
+                {
+                    if (m_currentFillTime >= m_timeToFill)
+                    {
+                        m_currentFillTime = 0;
+
+                        if (m_pipesToFill.Count == 0)
+                        {
+                            GetStartPipe();
+                        }
+
+                        int currentFillCount = m_pipesToFill.Count;
+
+                        for (int i = 0; i < currentFillCount; i++)
+                        {      
+                            bool endFilled = false;
+                            if (m_pipesToFill[i].m_location.x < 0 || m_pipesToFill[i].m_location.y >= m_gameBoard.Count || m_pipesToFill[i].m_location.y < 0)
+                            {
+                                m_hasLost = true;
+                            }
+                            else if (m_pipesToFill[i].m_location.x >= m_gameBoard.Count)
+                            {
+                                if (m_endTiles[(int)m_pipesToFill[i].m_location.y] != null)
+                                {
+                                    m_endTiles[(int)m_pipesToFill[i].m_location.y].PipeFill();
+                                    endFilled = true;
+
+                                    int endTiles = 0;
+                                    int filledEndTiles = 0;
+                                    for (int j = 0; j < m_endTiles.Count; j++)
+                                    {
+                                        if (m_endTiles[j] != null)
+                                        {
+                                            endTiles++;
+                                            if (m_endTiles[j].GetIsFull())
+                                            {
+                                                filledEndTiles++;
+                                            }
+                                        }
+                                    }
+
+                                    if (filledEndTiles >= endTiles)
+                                    {
+                                        m_hasWon = true;
+                                    }
+                                }
+                                else
+                                {
+                                    m_hasLost = true;
+                                }
+                            }
+                            if (!m_hasLost && !endFilled)
+                            {
+                                Tile currentTile = m_gameBoard[(int)m_pipesToFill[i].m_location.x].m_column[(int)m_pipesToFill[i].m_location.y];
+                                if (!currentTile.GetIsFull())
+                                {
+                                    if (CheckFrom(currentTile, m_pipesToFill[i].m_filledFrom))
+                                    {                                    
+                                        currentTile.PipeFill();
+
+                                        CheckTop(i);
+                                        CheckLeft(i);
+                                        CheckRight(i);
+                                        CheckBottom(i);
+                                    }
+                                    else
+                                    {
+                                        m_hasLost = true;
+                                    }
+                                }
+                            }
+                        }
+
+                        m_pipesToFill.RemoveRange(0, currentFillCount);
+                    }
+                    else
+                    {
+                        m_currentFillTime += Time.deltaTime;
+                    }
+                }
+                else
+                {
+                    if (m_startFillTime < m_timeToStart)
+                    {
+                        m_startFillTime += Time.deltaTime;
+                    }
+                    else
+                    {
+                        m_isFilling = true;
+                        m_currentFillTime = m_timeToFill;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (m_gameLostObject != null && !m_gameLostObject.activeInHierarchy)
+            {
+                m_gameLostObject.SetActive(true);
+            }
+        }
+    }
+
+    private void GetStartPipe()
+    {
+        for (int i = 0; i < m_gameBoard[0].m_column.Count; i++)
+        {
+            if (m_gameBoard[0].m_column[i].GetTileType() != Tile.TileType.Empty)
+            {
+                m_pipesToFill.Add(new PipesToFill(new Vector2(0, i), FillLocation.Left));
+
+                break;
+            }
+        }
+    }
+
+    private bool CheckFrom(Tile currentTile, FillLocation filledFrom)
+    {
+        if (filledFrom == FillLocation.Top && currentTile.GetTop())
+        {
+            currentTile.SetTop(false);
+            return true;
+        }
+        else if (filledFrom == FillLocation.Left && currentTile.GetLeft())
+        {
+            currentTile.SetLeft(false);
+            return true;
+        }
+        else if (filledFrom == FillLocation.Right && currentTile.GetRight())
+        {
+            currentTile.SetRight(false);
+            return true;
+        }
+        else if (filledFrom == FillLocation.Bottom && currentTile.GetBottom())
+        {
+            currentTile.SetBottom(false);
+            return true;
+        }
+        return false;
+    }
+
+    private void CheckTop(int i)
+    {
+        if (m_gameBoard[(int)m_pipesToFill[i].m_location.x].m_column[(int)m_pipesToFill[i].m_location.y].GetTop())
+        {       
+            m_pipesToFill.Add(new PipesToFill(new Vector2(m_pipesToFill[i].m_location.x, m_pipesToFill[i].m_location.y + 1), FillLocation.Bottom));
+        }
+    }
+
+    private void CheckLeft(int i)
+    {
+        if (m_gameBoard[(int)m_pipesToFill[i].m_location.x].m_column[(int)m_pipesToFill[i].m_location.y].GetLeft())
+        {       
+            m_pipesToFill.Add(new PipesToFill(new Vector2(m_pipesToFill[i].m_location.x - 1, m_pipesToFill[i].m_location.y), FillLocation.Right));
+        }
+    }
+
+    private void CheckRight(int i)
+    {
+        if (m_gameBoard[(int)m_pipesToFill[i].m_location.x].m_column[(int)m_pipesToFill[i].m_location.y].GetRight())
+        {       
+            m_pipesToFill.Add(new PipesToFill(new Vector2(m_pipesToFill[i].m_location.x + 1, m_pipesToFill[i].m_location.y), FillLocation.Left));
+        }
+    }
+
+    private void CheckBottom(int i)
+    {
+        if (m_gameBoard[(int)m_pipesToFill[i].m_location.x].m_column[(int)m_pipesToFill[i].m_location.y].GetBottom())
+        {       
+            m_pipesToFill.Add(new PipesToFill(new Vector2(m_pipesToFill[i].m_location.x, m_pipesToFill[i].m_location.y - 1), FillLocation.Top));
+        }
+    }
+
+    public void FastForward()
+    {
+        m_timeToStart = 0;
+        m_timeToFill = 0;
+    }
+}
