153472750a3a7530f11e1b154d9fecda277664c9
Lewis Nicholson <Lewies@live.co.uk>
2017-08-13 15:15:51 +0100

    Adding lives

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Net;
+using System.Net.Sockets;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null;
 
+    [Header("Levels")]
     [SerializeField] private int m_currentLevel = 0;
+
+    [Header("Lives")]
+    [SerializeField] private int m_currentLives;
+    [SerializeField] private int m_maxLives = 5;
+    [SerializeField] private int m_timeToLivesRefil = 600;
+    private int m_timeLivesSpent;
+    private double m_timeOffset = 0;
 
     private void Awake()
     {
@@ -23,6 +34,24 @@ public class GameManager : MonoBehaviour
         #if !UNITY_EDITOR
         m_currentLevel = PlayerPrefs.GetInt("ProgressLevel", 0);
         #endif
+
+        m_currentLives = PlayerPrefs.GetInt("CurrentLives", m_maxLives);
+        m_timeLivesSpent = PlayerPrefs.GetInt("TimeLivesSpent", 0);
+
+        var localTime = LocalTime();
+        var networkTime = GetNetworkTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+
+        m_timeOffset = localTime - networkTime;
+
+        CheckLives();
+    }
+
+    private void Update()
+    {
+        if (m_currentLives < m_maxLives)
+        {
+            CheckLives();
+        }
     }
 
     public int GetCurrentLevel()
@@ -43,5 +72,112 @@ public class GameManager : MonoBehaviour
         {
             PlayerPrefs.SetInt("ProgressLevel", m_currentLevel);
         }
+    }
+
+    public static DateTime GetNetworkTime()
+    {
+        try
+        {
+            //default Windows time server
+            const string ntpServer = "time.windows.com";
+
+            // NTP message size - 16 bytes of the digest (RFC 2030)
+            var ntpData = new byte[48];
+
+            //Setting the Leap Indicator, Version Number and Mode values
+            ntpData[0] = 0x1B; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
+
+            var addresses = Dns.GetHostEntry(ntpServer).AddressList;
+
+            //The UDP port number assigned to NTP is 123
+            var ipEndPoint = new IPEndPoint(addresses[0], 123);
+            //NTP uses UDP
+
+            using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+            {
+                socket.Connect(ipEndPoint);
+
+                //Stops code hang if NTP is blocked
+                socket.ReceiveTimeout = 3000;     
+
+                socket.Send(ntpData);
+                socket.Receive(ntpData);
+                socket.Close();
+            }
+
+            //Offset to get to the "Transmit Timestamp" field (time at which the reply 
+            //departed the server for the client, in 64-bit timestamp format."
+            const byte serverReplyTime = 40;
+
+            //Get the seconds part
+            ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
+
+            //Get the seconds fraction
+            ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
+
+            //Convert From big-endian to little-endian
+            intPart = SwapEndianness(intPart);
+            fractPart = SwapEndianness(fractPart);
+
+            var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+
+            //**UTC** time
+            var networkDateTime = (new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddMilliseconds((long)milliseconds);
+
+            return networkDateTime.ToLocalTime();
+        }
+        catch (Exception exception)
+        {
+            Debug.Log("Could not get network time");
+            Debug.Log(exception);
+            return DateTime.Now;
+        }
+    }
+
+    // stackoverflow.com/a/3294698/162671
+    static uint SwapEndianness(ulong x)
+    {
+        return (uint)(((x & 0x000000ff) << 24) +
+        ((x & 0x0000ff00) << 8) +
+        ((x & 0x00ff0000) >> 8) +
+        ((x & 0xff000000) >> 24));
+    }
+
+    public static double LocalTime()
+    {
+        return DateTime.Now.Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+    }
+
+    public void CheckLives()
+    {
+        double diff = (LocalTime() - m_timeOffset) - m_timeLivesSpent;
+        if (diff > m_timeToLivesRefil)
+        {
+            m_currentLives += (int)(diff / m_timeToLivesRefil);
+            m_timeLivesSpent = (int)(LocalTime() - m_timeOffset);
+
+            if (m_currentLives > m_maxLives)
+            {
+                m_currentLives = m_maxLives;
+            }
+
+            PlayerPrefs.SetInt("CurrentLives", m_currentLives);
+            PlayerPrefs.SetInt("TimeLivesSpent", m_timeLivesSpent);
+        }
+    }
+
+    public void SpendLives(int num)
+    {
+        m_currentLives -= num;
+        m_timeLivesSpent = (int)(LocalTime() - m_timeOffset);
+
+        if (m_currentLives < 0)
+        {
+            m_currentLives = 0;
+        }
+
+        PlayerPrefs.SetInt("CurrentLives", m_currentLives);
+
+        PlayerPrefs.SetInt("TimeLivesSpent", m_timeLivesSpent);
     }
 }
