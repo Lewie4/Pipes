416f8bf45a8a67d67f95a5b7ad1fa4bfbe803365
Lewis Nicholson <Lewies@live.co.uk>
2017-08-15 00:33:29 +0100

    Added scrollable menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private float m_snapSpeed = 10;
+
+    private RectTransform m_RT;
+    private ScrollRect m_scrollRect;
+
+    private Vector3 m_destination;
+
+    private void Awake()
+    {
+        m_RT = this.GetComponent<RectTransform>();
+        m_RT.sizeDelta = new Vector2(Screen.width * this.transform.childCount, Screen.height);
+
+        m_scrollRect = this.transform.parent.GetComponent<ScrollRect>();
+
+        m_destination = Vector3.one;
+    }
+
+    private void Update()
+    {
+        if (m_destination != Vector3.one)
+        {
+            m_RT.position = Vector3.Lerp(m_RT.position, m_destination, Time.deltaTime * m_snapSpeed);
+        }
+    }
+
+    public void DragEnd()
+    {
+        if ((Screen.width + m_RT.position.x) % Screen.width < (Screen.width - m_RT.position.x) % Screen.width)
+        {
+            m_scrollRect.velocity = Vector2.zero;
+            m_destination = new Vector3(m_RT.position.x - ((Screen.width + m_RT.position.x) % Screen.width), m_RT.position.y, 0);
+        }
+        else
+        {
+            m_scrollRect.velocity = Vector2.zero;
+            m_destination = new Vector3(m_RT.position.x + ((Screen.width - m_RT.position.x) % Screen.width), m_RT.position.y, 0);
+        }
+    }
+
+    public void DragStart()
+    {
+        m_destination = Vector3.one;
+    }
+}
