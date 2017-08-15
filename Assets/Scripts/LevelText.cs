b3ac3e2cd5ce1dcb9165bc03e02736446078b2c6
Matilde Herland <matildeherland@gmail.com>
2017-08-16 00:21:23 +0100

    Added new fore- and background to game scene and added level number to level scene

---
+++ Assets/Scripts/LevelText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelText : MonoBehaviour 
+{
+    [SerializeField] private Text m_text;
+
+	private void Start () 
+    {
+        if (m_text == null)
+        {
+            m_text = this.GetComponent<Text>();
+        }
+
+        if (m_text != null)
+        {
+            m_text.text = "Level " + (GameManager.Instance.GetCurrentLevel() + 1).ToString();
+        }
+	}
+}
