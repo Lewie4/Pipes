9e76f59cbca5d7714b9a7e34f05022064ae93c15
Lewis Nicholson <Lewies@live.co.uk>
2017-08-15 19:30:11 +0100

    Adding menu camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private RectTransform m_RT;
+
+    private bool m_startingLevel = false;
+
+    private void Update()
+    {
+        if (!m_startingLevel)
+        {
+            this.transform.position = new Vector3((m_RT.position.x / -1000), 0, 0);
+        }
+    }
+}
