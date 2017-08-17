5fed9d389f91d92c7e3a89c929994e999834a458
Lewis Nicholson <Lewies@live.co.uk>
2017-08-17 20:08:22 +0100

    Adding level screen locking

---
+++ Assets/Scripts/MenuLevelController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuLevelController : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> m_levelContainers;
+
+    private void Start()
+    {
+        int count = 0;
+
+        int unlockedLevel = PlayerPrefs.GetInt("ProgressLevel", 0);
+
+        for (int i = 0; i < m_levelContainers.Count; i++)
+        {
+            foreach (Transform child in m_levelContainers[i].transform)
+            {
+                child.GetComponent<Button>().interactable = (count < unlockedLevel);
+                
+                count++;
+            }
+        }
+    }
+}
