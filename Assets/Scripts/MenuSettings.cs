8b3dc416a5c6b3d252d9c73e80a257af40f61ba3
Lewis Nicholson <Lewies@live.co.uk>
2017-08-20 18:15:28 +0100

    Added mute button support and saving

--- Assets/Scripts/MenuSettings.cs
+++ Assets/Scripts/MenuSettings.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuSettings : MonoBehaviour
 {
+    [SerializeField] private Toggle m_audioToggle;
+
+    private void Start()
+    {
+        m_audioToggle.isOn = !SoundManager.Instance.GetMuted();
+    }
+
     public void SendEmail()
     {
         string email = "Lewies@live.co.uk";
@@ -15,5 +23,10 @@ public class MenuSettings : MonoBehaviour
     private string MyEscapeURL(string url)
     {
         return WWW.EscapeURL(url).Replace("+", "%20");
+    }
+
+    public void ToogleAudio(bool active)
+    {
+        SoundManager.Instance.MuteAudio(active);
     }
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -15,6 +15,8 @@ public class SoundManager : MonoBehaviour
     [SerializeField] private List<AudioClip> m_gameMusic;
     private int m_currentGameAudio = -1;
 
+    private bool m_muted = false;
+
     private void Awake()
     {
         if (Instance == null)
@@ -25,16 +27,56 @@ public class SoundManager : MonoBehaviour
         else if (Instance != this)
         {
             Destroy(this.gameObject);
-        }   
+        }  
+
+        if (PlayerPrefs.GetInt("AudioMuted", 0) == 0)
+        {
+            m_muted = false;
+        }
+        else
+        {
+            m_muted = true;
+        }
     }
 
     private void Start()
     {
         m_menuMusicSource.clip = m_menuMusic;
 
-        m_menuMusicSource.Play();
+        if (!m_muted)
+        {
+            m_menuMusicSource.Play();
+        }
 
         SceneManager.activeSceneChanged += PlayCorrectAudio;
+    }
+
+    public void MuteAudio(bool active)
+    {
+        m_muted = !active;
+
+        if (active)
+        {
+            PlayerPrefs.SetInt("AudioMuted", 0);
+            m_menuMusicSource.volume = 1;
+            m_gameMusicSource.volume = 1;
+
+            string sceneName = SceneManager.GetActiveScene().name;
+            if (sceneName == "MainMenu" && !m_menuMusicSource.isPlaying)
+            {
+                m_menuMusicSource.Play();
+            }
+            else if (sceneName == "Game" && !m_gameMusicSource.isPlaying)
+            {
+                m_gameMusicSource.Play();
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetInt("AudioMuted", 1);
+            m_menuMusicSource.volume = 0;
+            m_gameMusicSource.volume = 0;
+        }
     }
 
     private void PlayCorrectAudio(Scene prev, Scene current)
@@ -67,22 +109,30 @@ public class SoundManager : MonoBehaviour
         return audio[m_currentGameAudio];
     }
 
-    public static IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+    public bool GetMuted()
+    {
+        return m_muted;
+    }
+
+    public IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
     {
         float startVolume = audioSource.volume;
 
-        while (audioSource.volume > 0)
+        if (!m_muted)
         {
-            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+            while (audioSource.volume > 0)
+            {
+                audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
 
-            yield return null;
+                yield return null;
+            }
+        
+            audioSource.volume = startVolume;
         }
-
         audioSource.Stop();
-        audioSource.volume = startVolume;
     }
 
-    public static IEnumerator FadeIn(AudioSource audioSource, AudioClip audio, float FadeTime)
+    public IEnumerator FadeIn(AudioSource audioSource, AudioClip audio, float FadeTime)
     {
         float startVolume = 0.2f;
 
@@ -90,13 +140,25 @@ public class SoundManager : MonoBehaviour
         audioSource.volume = 0;
         audioSource.Play();
 
-        while (audioSource.volume < 1.0f)
+        if (!m_muted)
         {
-            audioSource.volume += startVolume * Time.deltaTime / FadeTime;
+            while (audioSource.volume < 1.0f)
+            {
+                if (m_muted)
+                {
+                    audioSource.volume = 0f;
+                    break;
+                }
+                audioSource.volume += startVolume * Time.deltaTime / FadeTime;
 
-            yield return null;
+                yield return null;
+            }
+
+            if (!m_muted)
+            {
+                audioSource.volume = 1f;
+            }
+
         }
-
-        audioSource.volume = 1f;
     }
 }
