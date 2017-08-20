4316cd75aa225633232270cdad8c3ab2fdd2f029
Lewis Nicholson <Lewies@live.co.uk>
2017-08-20 01:05:43 +0100

    Hooked up some of the settings menu

---
+++ Assets/Scripts/MenuSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSettings : MonoBehaviour
+{
+    public void SendEmail()
+    {
+        string email = "Lewies@live.co.uk";
+        string subject = MyEscapeURL("Pipes!");
+        string body = MyEscapeURL("");
+        Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+    }
+
+    private string MyEscapeURL(string url)
+    {
+        return WWW.EscapeURL(url).Replace("+", "%20");
+    }
+}
