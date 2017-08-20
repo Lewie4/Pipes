c73fac866d11728f8ea9dd339bd98e82d0d858ad
Lewis Nicholson <Lewies@live.co.uk>
2017-08-20 01:51:18 +0100

    AdsManager should now work with other platforms (HTML5)

--- Assets/Scripts/AdsManager.cs
+++ Assets/Scripts/AdsManager.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
+
+#if UNITY_ADS
 using UnityEngine.Advertisements;
+#endif
 using UnityEngine.Events;
 
 public class AdsManager : MonoBehaviour
@@ -40,15 +43,20 @@ public class AdsManager : MonoBehaviour
 
     public bool CheckRewardedAd()
     {
+        #if UNITY_ADS
         if (Advertisement.IsReady("rewardedVideo"))
         {
             return true;
         }
         return false;
+        #else
+        return true;
+        #endif
     }
 
     public void ShowRewardedAd(UnityEvent actions = null)
     {
+        #if UNITY_ADS
         if (Advertisement.IsReady("rewardedVideo"))
         {
             m_rewardCompleteActions = actions;
@@ -56,35 +64,43 @@ public class AdsManager : MonoBehaviour
             Advertisement.Show("rewardedVideo", options);
             Time.timeScale = 0;
         }
+        #else
+        AwardLives(1);
+        #endif
     }
 
     public bool CheckInterstitialAd()
     {
+        #if UNITY_ADS
         if (Advertisement.IsReady("video"))
         {
             return true;
         }
         return false;
+        #else
+        return true;
+        #endif
     }
 
     public void ShowInterstitialAd()
     {
+        #if UNITY_ADS
         if (Advertisement.IsReady("video"))
         {
             Advertisement.Show("video");
             Time.timeScale = 0;
         }
+        #endif
     }
 
+    #if UNITY_ADS
     private void HandleShowResult(ShowResult result)
     {
         switch (result)
         {
             case ShowResult.Finished:
                 Debug.Log("The ad was successfully shown.");
-                GameManager.Instance.GainLives(1);
-                m_rewardCompleteActions.Invoke();
-                m_rewardCompleteActions = null;
+                AwardLives(1);
                 break;
             case ShowResult.Skipped:
                 Debug.Log("The ad was skipped before reaching the end.");
@@ -94,13 +110,23 @@ public class AdsManager : MonoBehaviour
                 break;
         }
     }
+    #endif
+
+    public void AwardLives(int lives)
+    {
+        GameManager.Instance.GainLives(lives);
+        m_rewardCompleteActions.Invoke();
+        m_rewardCompleteActions = null;
+    }
 
     public bool CheckRandomInterstitialAd()
     {
+        #if UNITY_ADS
         if (Random.Range(0, 100) <= m_adChance && AdsManager.Instance.CheckInterstitialAd() && CheckInterstitialAd())
         {
             return true;
         }
+        #endif
         return false;
     }
 }
