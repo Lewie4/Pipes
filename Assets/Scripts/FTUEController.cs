f4676f435677fbbd6d4bb22977566cbbdd1ec710
Lewis Nicholson <Lewies@live.co.uk>
2017-08-26 15:28:18 +0100

    Updated the FTUE controller

--- Assets/Scripts/FTUEController.cs
+++ Assets/Scripts/FTUEController.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class FTUEController : MonoBehaviour {
+public class FTUEController : MonoBehaviour
+{
+    [System.Serializable]
+    public class FTUEStep
+    {
+        public int m_ftueLevel = -1;
+        public GameObject m_ftueObject;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private List<FTUEStep> m_FTUESteps;
+    private int m_currentLevel = -1;
+
+    private void Update()
+    {
+        if (m_currentLevel != GameManager.Instance.GetCurrentLevel())
+        {
+            m_currentLevel = GameManager.Instance.GetCurrentLevel();
+            for (int i = 0; i < m_FTUESteps.Count; i++)
+            {
+                if (m_FTUESteps[i].m_ftueLevel == m_currentLevel)
+                {
+                    m_FTUESteps[i].m_ftueObject.SetActive(true);
+                }
+                else
+                {
+                    m_FTUESteps[i].m_ftueObject.SetActive(false);
+                }
+            }
+        }
+    }
 }
