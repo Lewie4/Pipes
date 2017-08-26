48b4f31d2ea1dcb6bd0a33cb65a6dbd6d234a612
Lewis Nicholson <Lewies@live.co.uk>
2017-08-26 14:30:30 +0100

    Added moving clouds to the menu

---
+++ Assets/Scripts/CloudController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour
+{
+    [SerializeField] private float m_speed;
+
+    private void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x + (Time.deltaTime * m_speed), this.transform.position.y, this.transform.position.z);
+    }
+}
